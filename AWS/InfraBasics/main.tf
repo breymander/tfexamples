c22af4b9390c6087f3a1e574f42102e3c1b1fc25
Bryan Reymander <breymander@OnBase.net>
2018-09-19 15:04:07 -0400

    Added additional config.  Instances, Sec Groups, etc.

--- AWS/InfraBasics/main.tf
+++ AWS/InfraBasics/main.tf
@@ -1,5 +1,5 @@
 # Create a VPC to launch our instances into
-resource "aws_vpc" "default" {
+resource "aws_vpc" "tf_vpc" {
   cidr_block = "10.10.11.0/24"
   tags {
     Name = "TerraForm Testing"
@@ -7,11 +7,103 @@ resource "aws_vpc" "default" {
 }
 
 # Create a subnet to launch our instances into
-resource "aws_subnet" "default" {
-  vpc_id                  = "${aws_vpc.default.id}"
+resource "aws_subnet" "tf_subnet" {
+  vpc_id                  = "${aws_vpc.tf_vpc.id}"
   cidr_block              = "10.10.11.0/24"
   map_public_ip_on_launch = true
   tags {
     Name = "TerraForm Testing"
+  }
+}
+
+# IG to grant interweb access
+resource "aws_internet_gateway" "tf_gateway" {
+  vpc_id = "${aws_vpc.tf_vpc.id}"
+}
+
+# VPC interweb access
+resource "aws_route" "internet_access" {
+  route_table_id         = "${aws_vpc.tf_vpc.main_route_table_id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = "${aws_internet_gateway.tf_gateway.id}"
+}
+
+# A security group for the ELB so it is accessible via the web
+resource "aws_security_group" "tf_sg_elb" {
+  name        = "terraform_example_elb"
+  description = "Used in the terraform"
+  vpc_id      = "${aws_vpc.tf_vpc.id}"
+
+  # HTTP access from anywhere
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "tf_vpc_sg" {
+  name        = "terraform_example"
+  description = "Used in the terraform"
+  vpc_id      = "${aws_vpc.tf_vpc.id}"
+
+  # HTTP access from the VPC
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+}
+
+resource "aws_elb" "tf_elb_frontend" {
+  name = "TFFrontend"
+
+  subnets         = ["${aws_subnet.tf_subnet.id}"]
+  security_groups = ["${aws_security_group.tf_sg_elb.id}"]
+  instances       = ["${aws_instance.tf_frontend.id}"]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+}
+
+resource "aws_instance" "tf_frontend" {
+  # The connection block tells our provisioner how to
+  # communicate with the resource (instance)
+  connection {
+    # The default username for our AMI
+    user = "ubuntu"
+
+    # The connection will use the local SSH agent for authentication.
+  }
+
+  instance_type = "t2.micro"
+
+  # Lookup the correct AMI based on the region
+  # we specified
+  ami = "${lookup(var.aws_amis, var.aws_region)}"
+
+  # Our Security group to allow HTTP and SSH access
+  vpc_security_group_ids = ["${aws_security_group.tf_vpc_sg.id}"]
+
+  # We're going to launch into the same subnet as our ELB. In a production
+  # environment it's more common to have a separate private subnet for
+  # backend instances.
+  subnet_id = "${aws_subnet.tf_subnet.id}"
+
+  # We run a remote provisioner on the instance after creating it.
+  # In this case, we just install nginx and start it. By default,
+  # this should be on port 80
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get -y update",
+      "sudo apt-get -y install nginx",
+      "sudo service nginx start",
+    ]
   }
 }
---
+++ AWS/InfraBasics/variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  description = "AWS region to launch servers."
+  default     = "us-west-2"
+}
+
+# Ubuntu Precise 12.04 LTS (x64)
+variable "aws_amis" {
+  default = {
+    eu-west-1 = "ami-674cbc1e"
+    us-east-1 = "ami-1d4e7a66"
+    us-west-1 = "ami-969ab1f6"
+    us-west-2 = "ami-8803e0f0"
+  }
+}
